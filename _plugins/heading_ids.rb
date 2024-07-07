e630f9038c91825a01b606e0343e44db2db8227f
lorinbaum <79799805+lorinbaum@users.noreply.github.com>
2024-07-07 12:25:29 +0200

    delete jekyll remains, deletion for the win!

--- _plugins/heading_ids.rb
+++
@@ -1,7 +0,0 @@
-Jekyll::Hooks.register :posts, :post_render do |post|
-  post.output = post.output.gsub(/<h(\d) id="[^"]*?">(.*?)<\/h\1>/) do |match|
-    level = $1
-    text = $2
-    "<h#{level} id='#{text}'>#{text}</h#{level}>"
-  end
-end
